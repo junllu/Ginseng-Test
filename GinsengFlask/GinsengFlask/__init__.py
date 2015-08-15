169c234e3a534ebbdb719b1693791cb1f207e03b
junllu <MisoSoup@MacBooks-MacBook-Pro.local>
2015-08-14 23:52:11 -0700

    head and body reformat

--- GinsengFlask/GinsengFlask/__init__.py
+++ GinsengFlask/GinsengFlask/__init__.py
@@ -13,6 +13,13 @@ def homepage():
 def aboutpage():
     return render_template("index.html",title = "Information about this Site" , paragraph = "8554435484851321321321 blah blah")
 
+
+@app.route('/contact')
+def contactpage():
+	return "This is Contact Page"
+
+
+
 if __name__ == "__main__":
     app.run()
 
