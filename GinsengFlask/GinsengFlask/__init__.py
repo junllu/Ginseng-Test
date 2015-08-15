92e36b7a561f1f12d2a30bd09ae09a22cf14ab41
junllu <jllu@ucsd.edu>
2015-08-15 15:20:34 -0700

    Updated Jumbotron

--- GinsengFlask/GinsengFlask/__init__.py
+++ GinsengFlask/GinsengFlask/__init__.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template
 
 app = Flask(__name__)
+app.debug = True
 
 @app.route('/')
 def homepage():
@@ -12,15 +13,20 @@ def homepage():
 @app.route('/about')
 def aboutpage():
 	pageType = 'about'
-    return render_template("index.html",pageType = pageType, title = "Information about this Site" , paragraph = "8554435484851321321321 blah blah")
+	return render_template("index.html",pageType = pageType, title = "Information about this Site" , paragraph = "8554435484851321321321 blah blah")
 
 
 @app.route('/contact')
 def contactpage():
-	return "This is Contact Page"
+	return "This is Contact Page."
+
+
+@app.route('/ginseng')
+def ginsengpage():
+	return "Fuck yeaaaaa!"
 
 
 
 if __name__ == "__main__":
-    app.run()
+	app.run()
 
